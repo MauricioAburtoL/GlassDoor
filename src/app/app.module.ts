0d6e1294dc1845be2c81f791e7fb5bef7393a049
MauricioAburtoL <85853037+MauricioAburtoL@users.noreply.github.com>
2023-10-25 21:54:04 -0600

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BodyComponent } from './components/body/body.component';
+import { WidgetComponent } from './components/widget/widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    BodyComponent,
+    WidgetComponent
   ],
   imports: [
     BrowserModule,
